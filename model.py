3808f8c1db21fcea68be7aab3a498bbd88cc80af
jimmytran1 <92204677+jimmytran1@users.noreply.github.com>
2024-09-14 15:01:55 -0600

    Update model.py

--- model.py
+++ model.py
@@ -14,19 +14,6 @@ mnist = tf.keras.datasets.mnist
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-# # Creating a model (Sequential means it will be built step by step, layer by layer)
-# model = tf.keras.models.Sequential()
-
-# model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-# model.add(tf.keras.layers.Dense(128, activation='relu'))
-# model.add(tf.keras.layers.Dense(128, activation='relu'))
-# model.add(tf.keras.layers.Dense(10, activation='softmax'))
-
-# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-# model.fit(x_train, y_train, epochs=3)
-
-# model.save('handwritten_model.h5')
 
 model = tf.keras.models.load_model('handwritten_model.h5')
 
